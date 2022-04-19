05afeb233e3483ddf964dda81e84538a955ae09f
Karthik <jskarthik1@gmail.com>
2022-04-19 21:58:51 +0530

    Organized Folder Structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
 
 function ExpenseItem(props) {
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate info={props.info} />
             <div className='expense-item__description'>
                 <h2>{props.info.title}</h2>
                 <h2 className='expense-item__price'>${props.info.amount}</h2>
             </div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,15 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
+import Card from '../UI/Card'
+
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem info={props.items[0]}></ExpenseItem>
             <ExpenseItem info={props.items[1]}></ExpenseItem>
             <ExpenseItem info={props.items[2]}></ExpenseItem>
             <ExpenseItem info={props.items[3]}></ExpenseItem>
-        </div>
+        </Card>
     )
 }
 
